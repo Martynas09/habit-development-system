6588b86b0b4b3c7061b623c7a5399e788093257e
Martynas <79332444+Martynas09@users.noreply.github.com>
2023-05-07 17:21:30 +0300

    Update reminders.sh

--- reminders.sh
+++ reminders.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-set -e
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- php "$@"
-fi
-
-exec "$@"
 cd /var/www/html
-php artisan schedule:work
+php artisan schedule:work --verbose --no-interaction
+
