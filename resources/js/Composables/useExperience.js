624e366064f2819142c20786bf9432c545572850
Martynas <79332444+Martynas09@users.noreply.github.com>
2023-05-09 23:52:09 +0300

    Update useExperience.js

--- resources/js/Composables/useExperience.js
+++ resources/js/Composables/useExperience.js
@@ -5,7 +5,7 @@ const xp = ref(0);
 const level = ref(0);
 export default () => {
   const pullXP = async () => {
-    await axios.get('http://127.0.0.1:8000/getUserXp').then((response) => {
+    await axios.get('/getUserXp').then((response) => {
       xp.value = response.data.xp;
       level.value = response.data.level;
     });
